e0f323fc3ae377d502c9bc1f8ea9ec24b4935e78
Đức Mạnh <ducmanhlai22@gmail.com>
2024-02-24 15:33:43 +0000

    templates

--- server/server.py
+++ server/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     """Returns a simple 'Hello, world!' message."""
-    return 'Hello, world!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080,debug=True)
